53660cc23afc2e3a32e1c3e089f3915071e6113e
Artem Rudenko <artemr@compatibl.com>
2016-11-23 16:36:27 +0300

    test_boxed_stickers added

--- lesson3/test_task_2.py
+++ lesson3/test_task_2.py
@@ -1,11 +1,9 @@
 import pytest
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 url = 'http://localhost/litecart/en/'
+
 
 @pytest.fixture
 def driver(request):
@@ -16,7 +14,6 @@ def driver(request):
 
 
 def test_stickers_availability(driver):
-    wait = WebDriverWait(driver, 10)
     driver.get(url)
     for product in driver.find_elements(By.CSS_SELECTOR, 'li.product'):
         stickers = product.find_elements(By.CSS_SELECTOR, 'div.sticker')
@@ -25,3 +22,17 @@ def test_stickers_availability(driver):
         manufacturer = product.find_element(By.CSS_SELECTOR, '.manufacturer').text
         sticker = stickers[0].text
         print('Name: {name}, Manufacturer: {manufacturer} sticker is: {sticker}'.format(**locals()))
+
+
+def test_boxed_stickers(driver):
+    driver.get(url)
+    for boxLocator in ('box-most-popular', 'box-campaigns', 'box-latest-products'):
+        box = driver.find_element(By.ID, boxLocator)
+        print('Box: "{box_header}"'.format(box_header=box.find_element(By.CSS_SELECTOR, 'h3').text))
+        for product in box.find_elements(By.CSS_SELECTOR, 'li.product'):
+            stickers = product.find_elements(By.CSS_SELECTOR, 'div.sticker')
+            assert len(stickers) == 1, "One and only one sticker should be displayed for each item!"
+            name = product.find_element(By.CSS_SELECTOR, '.name').text
+            manufacturer = product.find_element(By.CSS_SELECTOR, '.manufacturer').text
+            sticker = stickers[0].text
+            print('Name: {name}, Manufacturer: {manufacturer} sticker is: {sticker}'.format(**locals()))
