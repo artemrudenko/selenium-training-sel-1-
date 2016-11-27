586e398edcc62e4a35b260dde57f8294b5a08aa7
Artem Rudenko <artem_rudenko@list.ru>
2016-11-27 19:04:45 +0300

    code refactoring

--- lesson4/test_task_1.py
+++ lesson4/test_task_1.py
@@ -1,12 +1,14 @@
+from contextlib import contextmanager
+
 import pytest
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 
 login, pwd = 'admin', 'admin'
+
+COUNTRIES_URL = 'http://localhost/litecart/admin/?app=countries&doc=countries'
+GEO_ZONES = 'http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones'
 
 
 class Locators(object):
@@ -14,7 +16,9 @@ class Locators(object):
     PWD = By.NAME, 'password'
     SUBMIT = By.NAME, 'login'
     TABLE = By.CLASS_NAME, 'dataTable'
-    ROW = By.CLASS_NAME, 'row'
+    TABLE_ZONES = By.ID, 'table-zones'
+    CANCEL = By.CSS_SELECTOR, '.button-set button[name="cancel"]'
+    NTH_ROW = By.CSS_SELECTOR, '.dataTable tr.row:nth-of-type({rowId})'
 
 
 @pytest.fixture
@@ -39,61 +43,65 @@ def getColumnIndex(table, text):
 
 
 def getColumnIndex2(table, text):
-    return len(table.find_elements(By.XPATH, './/th[contains(., "{text}")]/preceding-sibling::th'.format(text=text))) + 1
+    locator = './/th[contains(., "{text}")]/preceding-sibling::th'
+    return len(table.find_elements(By.XPATH, locator.format(text=text))) + 1
 
 
 def get_table_column_values(table, column, child=None):
     index = getColumnIndex2(table, column)
     assert index != -1, '{column} column was not found!'.format(column=column)
-    locator = 'tr.row td:nth-of-type({index})'.format(index=index) + (' ' + child) if child else ''
-    # if child is None:
+    locator = 'tr.row td:nth-of-type({index})'.format(index=index) + ((' ' + child) if child else '')
     return [el.text for el in table.find_elements(By.CSS_SELECTOR, locator)]
-    # else:
-    #     return [el.getAttribute('value') for el in table.find_elements(By.CSS_SELECTOR, locator)]
 
 
 def test_countries_sort(driver):
-    init(driver, 'http://localhost/litecart/admin/?app=countries&doc=countries')
-    table = driver.find_element(By.CSS_SELECTOR, '.dataTable')
+    init(driver, COUNTRIES_URL)
+    table = driver.find_element(*Locators.TABLE)
     countries = get_table_column_values(table, 'Name')
     assert countries == sorted(countries)
 
 
+@contextmanager
+def cancelling(drv, thing):
+    try:
+        yield thing
+    finally:
+        drv.find_element(*Locators.CANCEL).click()
+
+
+def verify_zone_sorting(drv, child=None):
+    tableZ = drv.find_element(*Locators.TABLE_ZONES)
+    zones = get_table_column_values(tableZ, 'Name', child)
+    assert zones == sorted(zones), "SubZones have to be displayed sorted"
+
+
 def test_multizones_countries_sort(driver):
-    init(driver, 'http://localhost/litecart/admin/?app=countries&doc=countries')
+    init(driver, COUNTRIES_URL)
     find_element, find_elements = driver.find_element, driver.find_elements
-    table = driver.find_element(By.CSS_SELECTOR, '.dataTable')
-    countryNameIndex = getColumnIndex2(table, 'Name')
-    # assert countryNameIndex != -1, 'Name column was not found!'
-    zonesIndex = getColumnIndex2(table, 'Zones')
-    assert zonesIndex != -1, 'Zones column was not found!'
-    rowsCount = len(table.find_elements(By.CSS_SELECTOR, 'tr.row'))
-    for idx in range(2, rowsCount+2):
-        row = find_element(By.CSS_SELECTOR, '.dataTable tr.row:nth-of-type('+str(idx)+')')
-        zones = row.find_element(By.CSS_SELECTOR, 'td:nth-of-type('+str(zonesIndex)+')').text
-        if int(zones) == 0:
-            continue
+    table = driver.find_element(*Locators.TABLE)
+    non_empty_zones = [i for i, v in enumerate(get_table_column_values(table, 'Zones')) if int(v) != 0]
+    countryNameIndex = str(getColumnIndex2(table, 'Name'))
+    for idx in non_empty_zones:
+        rowId = str(idx + 2)
+        row = find_element(By.CSS_SELECTOR, 'tr.row:nth-of-type(' + rowId + ')')
+        row.find_element(By.CSS_SELECTOR, 'td:nth-of-type(' + countryNameIndex + ') a').click()
         try:
-            row.find_element(By.CSS_SELECTOR, 'td:nth-of-type('+str(countryNameIndex)+') a').click()
-            tableZ = driver.find_element(By.ID, 'table-zones')
-            zones = get_table_column_values(tableZ, 'Name')
-            assert zones == sorted(zones), "SubZones have to be displayed sorted"
+            verify_zone_sorting(driver)
         finally:
-            find_element(By.CSS_SELECTOR, '.button-set button[name="cancel"]').click()
+            find_element(*Locators.CANCEL).click()
 
 
 def test_zones_sorting(driver):
-    init(driver, 'http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones')
+    init(driver, GEO_ZONES)
     find_element, find_elements = driver.find_element, driver.find_elements
-    table = driver.find_element(By.CSS_SELECTOR, '.dataTable')
-    countryNameIndex = getColumnIndex2(table, 'Name')
+    table = driver.find_element(*Locators.TABLE)
+    countryNameIndex = str(getColumnIndex2(table, 'Name'))
     rowsCount = len(table.find_elements(By.CSS_SELECTOR, 'tr.row'))
     for idx in range(2, rowsCount+2):
-        row = find_element(By.CSS_SELECTOR, '.dataTable tr.row:nth-of-type('+str(idx)+')')
+        rowId = str(idx)
+        row = find_element(By.CSS_SELECTOR, 'tr.row:nth-of-type(' + rowId + ')')
+        row.find_element(By.CSS_SELECTOR, 'td:nth-of-type(' + countryNameIndex + ') a').click()
         try:
-            row.find_element(By.CSS_SELECTOR, 'td:nth-of-type('+str(countryNameIndex)+') a').click()
-            tableZ = driver.find_element(By.ID, 'table-zones')
-            zones = get_table_column_values(tableZ, 'Name', 'select option:checked')
-            assert zones == sorted(zones), "SubZones have to be displayed sorted"
+            verify_zone_sorting(driver, 'select option:checked')
         finally:
-            find_element(By.CSS_SELECTOR, '.button-set button[name="cancel"]').click()
+            find_element(*Locators.CANCEL).click()
